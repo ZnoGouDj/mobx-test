ab86b31a7e623cff3e9aedb03597e2a3533413d9
Egor <znogoud3@gmail.com>
2022-09-13 20:25:11 +0200

    first commit

---
+++ src/store/counter.js
@@ -0,0 +1,23 @@
+import {
+    makeAutoObservable,
+    makeObservable
+} from 'mobx';
+
+class Counter {
+    count = 0;
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    increment() {
+        this.count = this.count + 1;
+        console.log('increment ', this.count);
+    }
+
+    decrement() {
+        this.count = this.count - 1;
+        console.log('decrement ', this.count);
+    }
+}
+
+export default new Counter();
